cc2c82baa589972cb2e22149b123ef4693ee15ba
Urgor Saaha <urgorka@gmail.com>
2017-02-17 13:18:36 +0200

    Fix

--- workers/OperationSystem.php
+++ workers/OperationSystem.php
@@ -8,6 +8,7 @@ class OperationSystem {
     protected $returnVar = 0;
 
     public function execute($command, array $params) {
+        $this->output = [];
         $params = array_map('escapeshellarg', $params);
         array_unshift($params, $command);
         $string = call_user_func_array('sprintf', $params);
